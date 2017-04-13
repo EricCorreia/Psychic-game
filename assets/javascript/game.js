90cecf1ae412b3fcc6a063cbbb97d610951b0117
Eric Correia <eric151888@gmail.com>
2017-04-13 17:04:37 -0400

    issues with JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,67 @@
+// declare choices, wins, losses, guesses, guessesLeft, guessedLetters, Letter
+var choices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+var wins = 0;
+
+var losses = 0;
+
+var guesses = 9;
+
+var guessesLeft = 9;
+
+var guessedLetters = [];
+
+var letterToGuess = null;
+
+// Calculations to figure otu if you guess correctly
+var computerGuess = choices[Math.floor(Math.random() * choices.length)];
+
+var updateGuessesLeft = function() {
+  document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
+};
+
+var updateLetterToGuess = function() {
+  this.letterToGuess = this.choices[Math.floor(Math.random() * this.choices.length)];
+};
+
+var updateGuessesSoFar = function() {
+  document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
+};
+
+// reset
+var reset = function() {
+  totalGuesses = 9;
+  guessesLeft = 9;
+  guessedLetters = [];
+  updateLetterToGuess();
+  updateGuessesLeft();
+  updateGuessesSoFar();
+}
+
+updateLetterToGuess();
+updateGuessesLeft();
+
+// If statement for if you win or loss it would calculate it up
+document.onkeyup = function(event) {
+    guessesLeft--;
+  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+  guessedLetters.push(userGuess);
+  updateGuessesLeft();
+  updateGuessesSoFar();
+
+        if (guessesLeft > 0){
+            if (userGuess == letterToGuess){
+                wins++;
+                document.querySelector('#wins').innerHTML = "Wins: " + wins;
+                alert("Yes, you are psychic!");
+                reset();
+            }
+        }else if(guessesLeft == 0){
+            losses++;
+            document.querySelector('#losses').innerHTML = "Losses: " + losses;
+            alert("Sorry, you're not psychic, maybe try again?"); 
+            reset();
+        }
+};
+
